d8eea316a0ae9fb7e2466e1cfcbac6b2fec1e752
Rayani Rocha <rocharayani@gmail.com>
2022-11-13 19:52:09 -0300

    em andamento

--- ColetandoDados/ColetandoDados/spiders/artigos.py
+++ ColetandoDados/ColetandoDados/spiders/artigos.py
@@ -2,21 +2,20 @@ import scrapy
 
 class ArtigoSpider(scrapy.Spider):
     name = 'artigos'
-    start_urls = ['https://www.sanarmed.com/artigos-cientificos/oleo-de-cartamo-carthamus-tinctorius-aumenta-os-niveis-de-colesterol-total-e-ldl-colesterol-em-modelo-experimental-de-sindrome-metabolica']
+    start_urls = ['https://www.scielo.br/j/csc/a/8pWRXdtnjTQ6cjWzZwmwKbB/?lang=en']
 
     def parse(self, response):
-        divs = response.css('.section')
-        #print(response.css('.section').get())
+        divs = response.css('.articleSection')
 
         for div in divs:
-            paragrafo = response.css('p*')
-            print(paragrafos)
+            #print(div.css('p::text').get())
+            artigo = div.css('p::text').get()
+            yield{
+                'artigo': artigo
+            }
 
-          #  paragrafo = div.css('.text::text').get()
-          #  autor = div.css('.author::text').get()
-           # tags = div.css('.tag::text').getall() #pegar todos os elementos
-            #yield{
-             #   'frase': frase,
-              #  'autor': autor,
-               #'tags': tags
-            #}
+
+
+
+
+
